27c68a41d3052310143b4902ae3732f5888b9878
r0han99 <r409067@gmail.com>
2020-08-17 17:28:42 +0530

    Added Setup.sh file for Linux installation

---
+++ linux/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -f pyramid ]] && [[ -f pyramid-help.py ]] && [[ -f pyramidpy.py ]]
+then    
+
+    echo "Initiating install sequence"
+    echo "You need root access for this procedure.Please enter your password correctly if prompted."
+    sudo echo "This a temp file created while entering sudo mode" > .testrootfile.txt
+    echo "Installing dependencies .. "
+    echo ""
+    sudo apt install python3-pip
+    sudo apt install tree
+    pip3 install pandas
+    echo ""
+    echo "Dependencies, Installed."
+    echo ""
+    echo "Adding Scripts to the PATH"
+    sudo mv pyramid-help.py pyramid pyramidpy.py /usr/local/bin/
+    echo ""
+    echo "Scripts added. Now *-pyramid-* is a command-line utility."
+    echo ""
+    echo "Exec. Pyramid to setup project-stack."
+    echo ""
+    pyramid
+    sudo rm .testrootfile.txt
+
+else
+    if [ -f /usr/local/bin/pyramid ]
+    then
+        echo "Pyramid-utility is already installed."
+    else
+        echo "Error [Incomplete requirements], reclone the repo if required."
+
+    fi
+fi
+
